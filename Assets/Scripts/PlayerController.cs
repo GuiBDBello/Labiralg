1705dbb69b7b7cf6487b892224f70222f5e9daa5
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-25 00:05:52 -0200

    Adicionado Rigidbody ao 'Player'; Criado script 'PlayerController.cs'

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody rb;
+
+    // Chamado no primeiro frame que o script é ativo
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Chamado antes de renderizar um frame
+    void Update ()
+    {
+        
+    }
+
+    // Chamado antes de realizar cálculos de física
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
