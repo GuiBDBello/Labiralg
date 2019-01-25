0f29de7666dafa43e5a7f94c61cf8881923f61d5
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-25 00:29:42 -0200

    Criado script 'CameraController.cs'

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Chamado antes de renderizar um frame, mas após processamento
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
