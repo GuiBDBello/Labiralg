74e99e5167362a8d6755a4f09158cb14669c43be
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-01-27 23:39:43 -0200

    Adicionado no 'Player' evento de colisão ao 'Pick Up'; Alterado Prefab do 'Pick Up'

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,8 +26,18 @@ public class PlayerController : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-	Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+	    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-        rb.AddForce(movement * speed);
+        rb.AddForce (movement * speed);
+    }
+
+    // Chamado quando ocorre uma colisão no objeto que esse script é atribuído
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive (false);
+        }
+        //Destroy(other.gameObject);
     }
 }
