654427f9521628661d82f16366250814fec4b986
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-02-07 21:22:46 -0200

    Implementado algoritmo DFS (Depth First Search) para criar o Labirinto

--- Assets/Scripts/Maze.cs
+++ Assets/Scripts/Maze.cs
@@ -25,12 +25,16 @@ public class Maze : MonoBehaviour
 
     private Vector3 initialPosition;
     private GameObject wallHolder;
-    // TODO: Tornar variável cells privada
-    public Cell[] cells;
-    // TODO: Tornar variável currentCell privada
-    public int currentCell = 0;
+    private Cell[] cells;
+    private int currentCell = 0;
     // TODO: Verificar a necessidade da variável totalCells (igual à cells)
     private int totalCells = 0;
+    private int visitedCells;
+    private bool startedBuilding = false;
+    private int currentNeighbour = 0;
+    private List<int> lastCells;
+    private int backingUp = 0;
+    private int wallToBreak = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -86,6 +90,9 @@ public class Maze : MonoBehaviour
 
     void CreateCells ()
     {
+        lastCells = new List<int>();
+        lastCells.Clear();
+        totalCells = xSize * zSize;
         int children = wallHolder.transform.childCount;
         GameObject[] allWalls = new GameObject[children];
         cells = new Cell[xSize * zSize];
@@ -102,20 +109,19 @@ public class Maze : MonoBehaviour
         // Vincula paredes às células
         for (int cellprocess = 0; cellprocess < cells.Length; cellprocess++)
         {
+            // Verifica se é a última célula da linha
+            if (termCount == xSize)
+            {
+                // Pula uma célula e zera a "coluna" (pula para a próxima coluna)
+                westEastProcess++;
+                termCount = 0;
+            }
+
             cells[cellprocess] = new Cell();
             cells[cellprocess].west = allWalls[westEastProcess];
             cells[cellprocess].south = allWalls[childProcess + ((xSize + 1) * zSize)];
 
-            // Verifica se é a última célula da linha
-            if (termCount == xSize)
-            {
-                // Pula uma célula e zera a "coluna"
-                westEastProcess += 2;
-                termCount = 0;
-            } else
-            {
-                westEastProcess++;
-            }
+            westEastProcess++;
             termCount++;
             childProcess++;
 
@@ -129,14 +135,65 @@ public class Maze : MonoBehaviour
 
     void CreateMaze()
     {
-        GiveMeNeighbour();
+        //if (visitedCells < totalCells)
+        while (visitedCells < totalCells)
+        {
+            if (startedBuilding)
+            {
+                GiveMeNeighbour();
+                // Se a célula vizinha não foi visitada, e a célula atual já foi, quebra o muro entre elas
+                if (cells[currentNeighbour].visited == false && cells[currentCell].visited == true)
+                {
+                    BreakWall();
+                    cells[currentNeighbour].visited = true;
+                    visitedCells++;
+                    lastCells.Add(currentCell);
+                    currentCell = currentNeighbour;
+                    // Restaura o valor de retorno se não encontra nenhum vizinho
+                    if (lastCells.Count > 0)
+                    {
+                        backingUp = lastCells.Count - 1;
+                    }
+                }
+            } else
+            {
+                // Escolhe uma célula aleatória para iniciar a construção do Labirinto
+                currentCell = Random.Range(0, totalCells);
+                cells[currentCell].visited = true;
+                visitedCells++;
+                startedBuilding = true;
+            }
+
+            // Chamada recursiva
+            //Invoke("CreateMaze", 0.0f);
+            Debug.Log("Finished");
+        }
+    }
+
+    void BreakWall()
+    {
+        switch (wallToBreak)
+        {
+            case 1:
+                Destroy(cells[currentCell].south);
+                break;
+            case 2:
+                Destroy(cells[currentCell].west);
+                break;
+            case 3:
+                Destroy(cells[currentCell].east);
+                break;
+            case 4:
+                Destroy(cells[currentCell].north);
+                break;
+        }
     }
 
     void GiveMeNeighbour()
     {
-        totalCells = xSize * zSize;
         int length = 0;
         int[] neighbours = new int[4];
+        int[] connectingWall = new int[4];
         int check = 0;
 
         // Verifica se está na última célula da linha
@@ -153,6 +210,7 @@ public class Maze : MonoBehaviour
             if (cells[currentCell - xSize].visited == false)
             {
                 neighbours[length] = currentCell - xSize;
+                connectingWall[length] = 1;
                 length++;
             }
         }
@@ -162,6 +220,7 @@ public class Maze : MonoBehaviour
             if (cells[currentCell - 1].visited == false)
             {
                 neighbours[length] = currentCell - 1;
+                connectingWall[length] = 2;
                 length++;
             }
         }
@@ -171,6 +230,7 @@ public class Maze : MonoBehaviour
             if (cells[currentCell + 1].visited == false)
             {
                 neighbours[length] = currentCell + 1;
+                connectingWall[length] = 3;
                 length++;
             } 
         }
@@ -180,15 +240,32 @@ public class Maze : MonoBehaviour
             if (cells[currentCell + xSize].visited == false)
             {
                 neighbours[length] = currentCell + xSize;
+                connectingWall[length] = 4;
                 length++;
             }
         }
-
         
-
+        /*
         for (int i = 0; i < length; i++)
         {
             Debug.Log(neighbours[i]);
+        }
+        */
+
+        // Busca um vizinho aleatório
+        if (length != 0)
+        {
+            int theChosenOne = Random.Range(0, length);
+            currentNeighbour = neighbours[theChosenOne];
+            wallToBreak = connectingWall[theChosenOne];
+        } else
+        {
+            // Ao encontrar um caminho sem-saída, faz o caminho inverso até encontrar uma saída
+            if (backingUp > 0)
+            {
+                currentCell = lastCells[backingUp];
+                backingUp--;
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Labiralg
+
+## PT-BR
+
+### Resumo
+
 Jogo de Labirinto com Geração Procedural
 
-## Referências
+### Referências
+
 - https://unity3d.com/pt/learn/tutorials/s/roll-ball-tutorial
+- http://flattutorials.blogspot.com/2015/02/lets-create-perfect-maze-generator.html#more
+
+
+## EN-US
