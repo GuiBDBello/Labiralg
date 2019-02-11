c1137225bcf0f9c9c0abb1df02fe0fb1523300b6
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-02-11 19:36:08 -0200

    Alterada a movimentação do 'Player'; Adicionado 'Dash' (LeftShift); Alterada a gravidade para -100

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,9 +23,10 @@ public class PlayerController : MonoBehaviour
     }
 
     // Chamado antes de renderizar um frame
-    void Update ()
-    {
-        
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     // Chamado antes de realizar cálculos de física
@@ -36,6 +38,13 @@ public class PlayerController : MonoBehaviour
 	    Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
         rb.AddForce (movement * speed);
+
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            rb.AddForce(movement * speed * 10f);
+            rb.mass = 50f;
+        } else {
+            rb.mass = 1f;
+        }
     }
 
     // Chamado quando ocorre uma colisão no objeto que esse script é atribuído
