85cef5e99c7f4add03c1b9a4e1f9b1175904feaf
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-02-21 00:02:40 -0300

    Adicionado pacote de ícones 'Flat Icoon'; Criado Script para alternar entre 'Menu Inicial' e 'Menu de Configurações'

---
+++ Assets/Scripts/UISettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISettings : MonoBehaviour {
+
+    public GameObject panelMain;
+    public GameObject panelSettings;
+
+    // Start is called before the first frame update
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    public void ButtonSettingsPressed() {
+        panelMain.GetComponent<RectTransform>().anchoredPosition = new Vector2(-500.0f, 500.0f);
+        panelSettings.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 0.0f);
+    }
+
+    public void ButtonBackPressed() {
+        panelMain.GetComponent<RectTransform>().anchoredPosition = new Vector2(0.0f, 0.0f);
+        panelSettings.GetComponent<RectTransform>().anchoredPosition = new Vector2(500.0f, -500.0f);
+    }
+}
