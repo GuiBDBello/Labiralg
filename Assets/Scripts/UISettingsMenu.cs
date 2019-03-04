8ceff0129839065ab1a9431601cc77c8bb64f2b6
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-04 12:32:12 -0300

    Corrigido erro no nome do Script 'UISettingsMenu'

--- Assets/Scripts/UISettingsMenu.cs
+++ Assets/Scripts/UISettingsMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
 
-public class UISettings : MonoBehaviour {
+public class UISettingsMenu : MonoBehaviour {
 
     public GameObject panelMain;
     public GameObject panelSettings;
