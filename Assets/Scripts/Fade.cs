14517b02f34755202c7468e5d3a0f0f8cf0a90cf
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-03-16 23:10:08 -0300

    Criada Animação de 'Fade' para troca de Cenas

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Fade : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private int LevelToLoad { get; set; }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FadeToLevel(1);
+        }
+    }
+
+    public void FadeToLevel(int levelIndex)
+    {
+        this.LevelToLoad = levelIndex;
+        animator.SetTrigger("Fade Out");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(this.LevelToLoad);
+    }
+}
