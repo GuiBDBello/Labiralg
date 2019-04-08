021e650d2dc13234384d7b59ab720f40b0284c58
Guilherme Brunetto Dall Bello <guilhermedallbello@hotmail.com>
2019-04-07 21:36:24 -0300

    Adicionado evento na Ãnimação 'Zoom'

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -3,15 +3,25 @@
 public class Zoom : MonoBehaviour
 {
     private Animator animator;
+    private Vector3 playerStartPosition;
+
+    public Maze maze;
 
     // Start is called before the first frame update
     private void Start()
     {
         animator = GetComponent<Animator>();
+        playerStartPosition = new Vector3(0.0f, 0.5f, 0.0f);
     }
 
     public void ChangeZoom()
     {
         animator.SetTrigger(Animations.Zoom);
+    }
+
+    public void OnZoomComplete()
+    {
+        GameObject.FindGameObjectWithTag(Tags.Player).GetComponent<Rigidbody>().transform.position = playerStartPosition;
+        maze.NextMaze();
     }
 }
